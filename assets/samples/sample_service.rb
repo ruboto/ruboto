5c8aac6175e5f3825d89ac6fbe3919380f6bada9
Daniel Jackoway <danjdel@gmail.com>
2010-08-25 01:47:42 -0400

    give a note about how I'm not doing a service demo right now and give a little explanation of what to do

--- assets/samples/sample_service.rb
+++ assets/samples/sample_service.rb
@@ -1,4 +1,13 @@
 require 'ruboto.rb'
 $service.handle_create do
-  # define what your service does
+  # define what your service does. Directly put any code that you want
+  # executed when onCreate gets called. Define the rest of the
+  # behavior with handle_ blocks. See the README for more info.
+
+  # Services are complicated and don't really make sense unless you
+  # show the interaction between the Service and other parts of your
+  # app
+  # For now, just take a look at the explanation and example in
+  # online:
+  # http://developer.android.com/reference/android/app/Service.html
 end
