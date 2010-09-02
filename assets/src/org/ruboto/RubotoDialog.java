919a2c7b4cfe1526b5ded4320e6e1ba4f8e5f6f6
rscottm <rscottmoyer@gmail.com>
2010-09-01 22:09:05 -0700

    Added RubotoDialog.java back in

---
+++ assets/src/org/ruboto/RubotoDialog.java
@@ -0,0 +1,11 @@
+package org.ruboto;
+
+/***********************************************************
+ * 
+ * Extends RubotoActivity to specify Theme.Dialog in
+ * the AndroidManifest.xml file.
+ *
+ */
+
+public class RubotoDialog extends RubotoActivity {
+}
