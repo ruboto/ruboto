4c2cf7bcfcbe22ae9bc0d51059859e551bb9a915
rscottm <rscottmoyer@gmail.com>
2010-11-13 15:06:20 -0800

    Template for generating subclasses and interfaces

---
+++ assets/src/InheritingClass.java
@@ -0,0 +1,26 @@
+package THE_PACKAGE;
+
+import org.jruby.Ruby;
+import org.jruby.javasupport.util.RuntimeHelpers;
+import org.jruby.runtime.builtin.IRubyObject;
+import org.jruby.javasupport.JavaUtil;
+import org.jruby.exceptions.RaiseException;
+import org.ruboto.Script;
+
+public class THE_RUBOTO_CLASS THE_ACTION THE_ANDROID_CLASS {
+  private Ruby __ruby__;
+
+THE_CONSTANTS
+  private IRubyObject[] callbackProcs = new IRubyObject[CONSTANTS_COUNT];
+
+  private Ruby getRuby() {
+    if (__ruby__ == null) __ruby__ = Script.getRuby();
+    return __ruby__;
+  }
+
+  public void setCallbackProc(int id, IRubyObject obj) {
+    callbackProcs[id] = obj;
+  }
+	
+THE_METHODS
+}
