92a7d37c5e1a268c519a79c3d2256a8a3d44a06e
rscottm <rscottmoyer@gmail.com>
2010-11-19 10:57:51 -0800

    template for generating RubotoService

---
+++ assets/src/RubotoService.java
@@ -0,0 +1,75 @@
+package THE_PACKAGE;
+
+import org.jruby.Ruby;
+import org.jruby.javasupport.util.RuntimeHelpers;
+import org.jruby.runtime.builtin.IRubyObject;
+import org.jruby.javasupport.JavaUtil;
+import org.jruby.exceptions.RaiseException;
+import org.ruboto.Script;
+import java.io.IOException;
+import android.app.ProgressDialog;
+
+public abstract class THE_RUBOTO_CLASS THE_ACTION THE_ANDROID_CLASS {
+  private Ruby __ruby__;
+  private String scriptName;
+  private String remoteVariable = "";
+  public Object[] args;
+
+THE_CONSTANTS
+  private IRubyObject[] callbackProcs = new IRubyObject[CONSTANTS_COUNT];
+
+  private Ruby getRuby() {
+    if (__ruby__ == null) __ruby__ = Script.getRuby();
+
+    if (__ruby__ == null) {
+      Script.setUpJRuby(null);
+      __ruby__ = Script.getRuby();
+    }
+
+    return __ruby__;
+  }
+
+  public void setCallbackProc(int id, IRubyObject obj) {
+    callbackProcs[id] = obj;
+  }
+	
+  public THE_RUBOTO_CLASS setRemoteVariable(String var) {
+    remoteVariable = ((var == null) ? "" : (var + "."));
+    return this;
+  }
+
+  public void setScriptName(String name){
+    scriptName = name;
+  }
+
+  /****************************************************************************************
+   * 
+   *  Activity Lifecycle: onCreate
+   */
+	
+  @Override
+  public void onCreate() {
+    args = new Object[0];
+
+    super.onCreate();
+
+    getRuby();
+
+    Script.defineGlobalVariable("$service", this);
+
+    try {
+      new Script(scriptName).execute();
+    } catch(IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /****************************************************************************************
+   * 
+   *  Generated Methods
+   */
+
+THE_METHODS
+}	
+
+
