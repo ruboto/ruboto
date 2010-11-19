c28925185f7b0ea1fb923fc9865a71adafa38493
rscottm <rscottmoyer@gmail.com>
2010-11-19 10:57:34 -0800

    template for generating RubotoActivity

---
+++ assets/src/RubotoActivity.java
@@ -0,0 +1,93 @@
+package THE_PACKAGE;
+
+import org.jruby.Ruby;
+import org.jruby.javasupport.util.RuntimeHelpers;
+import org.jruby.runtime.builtin.IRubyObject;
+import org.jruby.javasupport.JavaUtil;
+import org.jruby.exceptions.RaiseException;
+import org.ruboto.Script;
+import java.io.IOException;
+import android.app.ProgressDialog;
+
+public class THE_RUBOTO_CLASS THE_ACTION THE_ANDROID_CLASS {
+  private Ruby __ruby__;
+  private String scriptName;
+  private String remoteVariable = "";
+  public Object[] args;
+
+THE_CONSTANTS
+  private IRubyObject[] callbackProcs = new IRubyObject[CONSTANTS_COUNT];
+
+  private Ruby getRuby() {
+    if (__ruby__ == null) __ruby__ = Script.getRuby();
+
+    if (__ruby__ == null) {
+      Script.setUpJRuby(null);
+      __ruby__ = Script.getRuby();
+    }
+
+    return __ruby__;
+  }
+
+  public void setCallbackProc(int id, IRubyObject obj) {
+    callbackProcs[id] = obj;
+  }
+	
+  public THE_RUBOTO_CLASS setRemoteVariable(String var) {
+    remoteVariable = ((var == null) ? "" : (var + "."));
+    return this;
+  }
+
+  public void setScriptName(String name){
+    scriptName = name;
+  }
+
+  /****************************************************************************************
+   * 
+   *  Activity Lifecycle: onCreate
+   */
+	
+  @Override
+  public void onCreate(android.os.Bundle arg0) {
+    args = new Object[1];
+    args[0] = arg0;
+
+    super.onCreate(arg0);
+
+    Script.copyScriptsIfNeeded(getFilesDir().getAbsolutePath() + "/scripts", getAssets());
+
+    getRuby();
+
+    Script.defineGlobalVariable("$activity", this);
+    Script.defineGlobalVariable("$bundle", arg0);
+
+    android.os.Bundle configBundle = getIntent().getBundleExtra("RubotoActivity Config");
+
+    if (configBundle != null) {
+      setRemoteVariable(configBundle.getString("Remote Variable"));
+      if (configBundle.getBoolean("Define Remote Variable")) {
+        Script.defineGlobalVariable(configBundle.getString("Remote Variable"), this);
+        setRemoteVariable(configBundle.getString("Remote Variable"));
+      }
+      if (configBundle.getString("Initialize Script") != null) {
+        Script.execute(configBundle.getString("Initialize Script"));
+      }
+      Script.execute(remoteVariable + "on_create($bundle)");
+    } else {
+      try {
+        new Script(scriptName).execute();
+      } catch(IOException e){
+        e.printStackTrace();
+        ProgressDialog.show(this, "Script failed", "Something bad happened", true, false);
+      }
+    }
+  }
+
+  /****************************************************************************************
+   * 
+   *  Generated Methods
+   */
+
+THE_METHODS
+}	
+
