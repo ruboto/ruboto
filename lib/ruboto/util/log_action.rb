78fb1b4e335564a4308992f48f1fbf0901092af3
rscottm <rscottmoyer@gmail.com>
2011-03-17 12:58:37 -0700

    Set up log_action to flush the first text before the action begins

--- lib/ruboto/util/log_action.rb
+++ lib/ruboto/util/log_action.rb
@@ -7,6 +7,8 @@ module Ruboto
       #
       
       def log_action(initial_text, final_text="Done.", &block)
+        $stdout.sync = true
+
         print initial_text, "..."
         result = yield
         puts final_text
