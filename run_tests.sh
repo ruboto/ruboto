2aa7540b48eee43c88b2d77160397472c30814ce
donv <uwe@kubosch.no>
2011-07-08 01:21:43 +0200

    * Removed jruby-jars-1.6.3.dev.gem since 1.6.3 has been released and we now build against JRuby master dist

--- run_tests.sh
+++ run_tests.sh
@@ -6,9 +6,6 @@ if [ -e /usr/local/jruby ] ; then
 elif [ -e /Library/Frameworks/JRuby.framework/Versions/Current ] ; then
   export JRUBY_HOME=/Library/Frameworks/JRuby.framework/Versions/Current
   export PATH=$JRUBY_HOME/bin:$PATH
-  env
-  which gem
-  which rake
 fi
 unset GEM_HOME
 if [ "$JRUBY_JARS_VERSION" != "" ] ; then
