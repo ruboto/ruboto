16ea1f6db5b72f755fc152807119de0ee87e7f90
donv <uwe@kubosch.no>
2011-07-10 23:46:09 +0200

    * Kill timeout child process to avoid warning in Jenkins

--- run_tests.sh
+++ run_tests.sh
@@ -13,6 +13,7 @@ echo "PIDs: Boss: $BOSSPID, Timer: $TIMERPID"
 trap "kill -9 $TIMERPID" EXIT
 # END TIMEOUT #
 
+
 if [ -e /usr/local/jruby ] ; then
   export JRUBY_HOME=/usr/local/jruby
   export PATH=$JRUBY_HOME/bin:$PATH
@@ -24,6 +25,11 @@ unset GEM_HOME
 bundle install --system
 if [ "$JRUBY_JARS_VERSION" != "" ] ; then
   gem install -v "$JRUBY_JARS_VERSION" jruby-jars
-  gem uninstall jruby-jars --all -v "!=$JRUBY_JARS_VERSION" || echo -n "" 
+  (gem uninstall jruby-jars --all -v "!=$JRUBY_JARS_VERSION") || echo "No superflous jruby-jars gem uninstalled." 
 fi
 rake --trace test
+
+
+# BEGIN TIMEOUT #
+kill -9 $TIMERPID
+# END TIMEOUT #
