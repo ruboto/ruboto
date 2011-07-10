4cf20b442eb103f0d0f21b8ae5f94ac7b5b7dbd2
donv <uwe@kubosch.no>
2011-07-10 18:58:25 +0200

    * Added timeout to run_tests.sh to avoid hung jobs

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -e
 
+# BEGIN TIMEOUT #
+TIMEOUT="1800"
+BOSSPID=$$
+(
+  sleep $TIMEOUT
+  kill -9 -$BOSSPID
+)&
+TIMERPID=$!
+
+trap "kill -9 $TIMERPID" EXIT
+# END TIMEOUT #
+
 if [ -e /usr/local/jruby ] ; then
   export JRUBY_HOME=/usr/local/jruby
   export PATH=$JRUBY_HOME/bin:$PATH
@@ -10,6 +22,7 @@ fi
 unset GEM_HOME
 bundle install --system
 if [ "$JRUBY_JARS_VERSION" != "" ] ; then
-  gem uninstall jruby-jars --all -v "!=$JRUBY_JARS_VERSION" && gem install -v "$JRUBY_JARS_VERSION" jruby-jars
+  gem install -v "$JRUBY_JARS_VERSION" jruby-jars
+  gem uninstall jruby-jars --all -v "!=$JRUBY_JARS_VERSION" || echo -n "" 
 fi
 rake --trace test
