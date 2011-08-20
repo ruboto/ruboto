fde76b7e72ccca39147b997cfb9b087690dcb57f
donv <uwe@kubosch.no>
2011-08-20 22:31:32 +0200

    * Added support for rubygems 1.8 in test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,11 @@ module RubotoTest
   PROJECT_DIR = File.expand_path('..', File.dirname(__FILE__))
   $LOAD_PATH << PROJECT_DIR
 
-  gem_spec = Gem.searcher.find('jruby-jars')
+  if Gem::Version.new(Gem::VERSION) >= Gem::Version.new('1.8.0')
+    gem_spec = Gem::Specification.find_by_path 'jruby-jars'
+  else
+    gem_spec = Gem.searcher.find('jruby-jars')
+  end
   raise StandardError.new("Can't find Gem specification jruby-jars.") unless gem_spec
   JRUBY_JARS_VERSION  = gem_spec.version
   ON_JRUBY_JARS_1_5_6 = JRUBY_JARS_VERSION == Gem::Version.new('1.5.6')
