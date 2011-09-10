dea14f08ce236ab95d7af6c96aa35cc0dec45c18
rscottm <rscottmoyer@gmail.com>
2011-09-10 00:45:17 -0700

    Revert last change until I can figure out why it broke the tests

--- assets/samples/sample_activity.rb
+++ assets/samples/sample_activity.rb
@@ -6,14 +6,16 @@ $activity.handle_create do |bundle|
   setTitle 'This is the Title'
 
   setup_content do
-    linear_layout :orientation => :vertical do
+    linear_layout :orientation => LinearLayout::VERTICAL do
       @text_view = text_view :text => 'What hath Matz wrought?', :id => 42
-      button :text => 'M-x butterfly', :width => :wrap_content, :id => 43, :on_click_listener => @handle_click
+      button :text => 'M-x butterfly', :width => :wrap_content, :id => 43
     end
   end
 
-  @handle_click = proc do |view|
-    @text_view.text = 'What hath Matz wrought!'
-    toast 'Flipped a bit via butterfly'
+  handle_click do |view|
+    if view.getText == 'M-x butterfly'
+      @text_view.setText 'What hath Matz wrought!'
+      toast 'Flipped a bit via butterfly'
+    end
   end
 end
