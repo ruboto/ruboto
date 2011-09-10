0137994a4e71ff7169aa88d507a58c75ceca5c82
rscottm <rscottmoyer@gmail.com>
2011-09-10 11:49:38 +0800

    Switched handle call for proc; other minor changes

--- assets/samples/sample_activity.rb
+++ assets/samples/sample_activity.rb
@@ -6,16 +6,14 @@ $activity.handle_create do |bundle|
   setTitle 'This is the Title'
 
   setup_content do
-    linear_layout :orientation => LinearLayout::VERTICAL do
+    linear_layout :orientation => :vertical do
       @text_view = text_view :text => 'What hath Matz wrought?', :id => 42
-      button :text => 'M-x butterfly', :width => :wrap_content, :id => 43
+      button :text => 'M-x butterfly', :width => :wrap_content, :id => 43, :on_click_listener => @handle_click
     end
   end
 
-  handle_click do |view|
-    if view.getText == 'M-x butterfly'
-      @text_view.setText 'What hath Matz wrought!'
-      toast 'Flipped a bit via butterfly'
-    end
+  @handle_click = proc do |view|
+    @text_view.text = 'What hath Matz wrought!'
+    toast 'Flipped a bit via butterfly'
   end
 end
