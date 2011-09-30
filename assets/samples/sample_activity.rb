6a9355da9e02d40713b15f4e8f5046c47ff38a14
rscottm <rscottmoyer@gmail.com>
2011-09-30 13:13:48 +0800

    Update sample to match new ruboto split

--- assets/samples/sample_activity.rb
+++ assets/samples/sample_activity.rb
@@ -1,21 +1,23 @@
-require 'ruboto'
+require 'ruboto/activity'
+require 'ruboto/widget'
+require 'ruboto/util/toast'
 
 ruboto_import_widgets :Button, :LinearLayout, :TextView
 
-$activity.handle_create do |bundle|
+$activity.start_ruboto_activity "$sample_activity" do
   setTitle 'This is the Title'
 
-  setup_content do
-    linear_layout :orientation => LinearLayout::VERTICAL do
-      @text_view = text_view :text => 'What hath Matz wrought?', :id => 42
-      button :text => 'M-x butterfly', :width => :wrap_content, :id => 43
-    end
+  def on_create(bundle)
+    self.content_view =
+        linear_layout(:orientation => :vertical) do
+          @text_view = text_view :text => 'What hath Matz wrought?', :id => 42
+          button :text => 'M-x butterfly', :width => :wrap_content, :id => 43,
+                    :on_click_listener => @handle_click
+        end
   end
 
-  handle_click do |view|
-    if view.getText == 'M-x butterfly'
-      @text_view.setText 'What hath Matz wrought!'
+  @handle_click = proc do |view|
+      @text_view.text = 'What hath Matz wrought!'
       toast 'Flipped a bit via butterfly'
-    end
   end
 end
