4731e74d4802888756a57e0010acd586ddee9e60
rscottm <rscottmoyer@gmail.com>
2011-09-30 13:15:53 +0800

    Update sample to match new ruboto split

--- assets/samples/sample_broadcast_receiver.rb
+++ assets/samples/sample_broadcast_receiver.rb
@@ -1,6 +1,9 @@
-require 'ruboto'
+require 'ruboto/broadcast_receiver'
 
 # will get called whenever the BroadcastReceiver receives an intent (whenever onReceive is called)
-$broadcast_receiver.handle_receive do |context, intent|
-  Log.v "MYAPP", intent.getExtras.to_s
+RubotoBroadcastReceiver.new_with_callbacks do
+  def on_receive(context, intent)
+    Log.v "MYAPP", intent.getExtras.to_s
+  end
 end
+
