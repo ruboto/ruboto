dbbdfc477cca5961e9fa89b3627f909ab34a3fd3
rscottm <rscottmoyer@gmail.com>
2011-09-30 13:15:21 +0800

    Update to work through manifest and directly

--- assets/src/RubotoBroadcastReceiver.java
+++ assets/src/RubotoBroadcastReceiver.java
@@ -1,7 +1,9 @@
 package THE_PACKAGE;
 
+import java.io.IOException;
+
 public class THE_RUBOTO_CLASS THE_ACTION THE_ANDROID_CLASS {
-    private String scriptName;
+    private String scriptName = null;
     private boolean initialized = false;
 
     public void setCallbackProc(int id, Object obj) {
@@ -20,26 +22,35 @@ public class THE_RUBOTO_CLASS THE_ACTION THE_ANDROID_CLASS {
     public THE_RUBOTO_CLASS(String name) {
         super();
 
-        if (name != null)
+        if (name != null) {
             setScriptName(name);
+        
+            if (Script.isInitialized()) {
+                loadScript();
+            }
+        }
+    }
+
+    protected void loadScript() {
+        Script.put("$broadcast_receiver", this);
+        if (scriptName != null) {
+            try {
+                new Script(scriptName).execute();
+            } catch(IOException e) {
+                throw new RuntimeException("IOException loading broadcast receiver script", e);
+            }
+        }
     }
 
     public void onReceive(android.content.Context context, android.content.Intent intent) {
-        if (Script.setUpJRuby(context)) {
-            Script.defineGlobalVariable("$context", context);
-            Script.defineGlobalVariable("$broadcast_receiver", this);
-            Script.defineGlobalVariable("$intent", intent);
+        Script.put("$context", context);
+        Script.put("$broadcast_receiver", this);
+        Script.put("$intent", intent);
 
-            try {
-                if (scriptName != null && !initialized) {
-                    new Script(scriptName).execute();
-                    initialized = true;
-                } else {
-                    Script.execute("$broadcast_receiver.on_receive($context, $intent)");
-                }
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
+        try {
+            Script.execute("$broadcast_receiver.on_receive($context, $intent)");
+        } catch(Exception e) {
+            e.printStackTrace();
         }
     }
 }	
