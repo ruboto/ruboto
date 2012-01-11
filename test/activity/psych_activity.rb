a9745cc9164345e766226cb815de31858613f93e
Uwe Kubosch <uwe@kubosch.no>
2012-01-11 10:24:58 +0100

    * Skip PSych test for jruby-jars 1.5.6 since it is not present

--- test/activity/psych_activity.rb
+++ test/activity/psych_activity.rb
@@ -1,31 +1,29 @@
-require 'jruby'
-require 'rbconfig'
-org.jruby.ext.psych.PsychLibrary.new.load(JRuby.runtime, false)
-$LOADED_FEATURES << 'psych.so'
-$LOAD_PATH << File.join(Config::CONFIG['libdir'], 'ruby/1.9')
-require 'psych.rb'
+# TODO(uwe): Remove check when we stop supporting jruby-jars 1.5.6
+if JRUBY_VERSION != '1.5.6'
+  require 'jruby'
+  require 'rbconfig'
+  org.jruby.ext.psych.PsychLibrary.new.load(JRuby.runtime, false)
+  $LOADED_FEATURES << 'psych.so'
+  $LOAD_PATH << File.join(Config::CONFIG['libdir'], 'ruby/1.9')
+  require 'psych.rb'
 
-Psych::Parser
-Psych::Handler
+  Psych::Parser
+  Psych::Handler
 
-#class String
-#  def encoding
-#    'ASCII'
-#  end
-#end
+  require 'ruboto'
 
-require 'ruboto'
+  ruboto_import_widgets :Button, :LinearLayout, :TextView
 
-ruboto_import_widgets :Button, :LinearLayout, :TextView
+  $activity.handle_create do |bundle|
+    setTitle File.basename(__FILE__).chomp('_activity.rb').split('_').map { |s| "#{s[0..0].upcase}#{s[1..-1]}" }.join(' ')
 
-$activity.handle_create do |bundle|
-  setTitle File.basename(__FILE__).chomp('_activity.rb').split('_').map { |s| "#{s[0..0].upcase}#{s[1..-1]}" }.join(' ')
-
-  setup_content do
-    linear_layout :orientation => LinearLayout::VERTICAL do
-      @decoded_view = text_view :id => 42, :text => Psych.load('--- foo')
-      # @encoded_view = text_view :id => 43, :text => Psych.dump("foo")
+    setup_content do
+      linear_layout :orientation => LinearLayout::VERTICAL do
+        @decoded_view = text_view :id => 42, :text => Psych.load('--- foo')
+        # @encoded_view = text_view :id => 43, :text => Psych.dump("foo")
+      end
     end
+
   end
 
 end
--- test/activity/psych_activity_test.rb
+++ test/activity/psych_activity_test.rb
@@ -1,16 +1,19 @@
-activity org.ruboto.test_app.PsychActivity
+# TODO(uwe): Remove check when we stop supporting jruby-jars 1.5.6
+if JRUBY_VERSION != '1.5.6'
+  activity org.ruboto.test_app.PsychActivity
 
-setup do |activity|
-  start = Time.now
-  loop do
-    @text_view = activity.findViewById(42)
-    break if @text_view || (Time.now - start > 60)
-    sleep 1
+  setup do |activity|
+    start = Time.now
+    loop do
+      @text_view = activity.findViewById(42)
+      break if @text_view || (Time.now - start > 60)
+      sleep 1
+    end
+    assert @text_view
   end
-  assert @text_view
-end
 
-test('psych_encode_decode') do |activity|
-  assert_equal 'foo', activity.find_view_by_id(42).text.to_s
-  #assert_equal "--- foo\n...\n", activity.find_view_by_id(43).text.to_s
+  test('psych_encode_decode') do |activity|
+    assert_equal 'foo', activity.find_view_by_id(42).text.to_s
+    #assert_equal "--- foo\n...\n", activity.find_view_by_id(43).text.to_s
+  end
 end
