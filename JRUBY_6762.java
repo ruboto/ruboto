3e9c69df49c4a9bf17af5b8d5e0ea6468995ae0e
Uwe Kubosch <uwe@kubosch.no>
2012-07-19 18:26:08 +0200

    * Added examples for reproducing bugs reported to JRuby JIRA.

--- JRUBY_6762.java
+++ JRUBY_6762.java
@@ -1,33 +1,33 @@
 import org.jruby.embed.ScriptingContainer;
 
-class MethodsExample {
+class JRUBY_6762 {
     public void myJavaMethod(String message) {
         System.out.println("myJavaMethod called: " + message);
     }
 
     public static void main(String[] args) {
         ScriptingContainer sc = new ScriptingContainer();
-        MethodsExample me = new MethodsExample();
+        JRUBY_6762 me = new JRUBY_6762();
 
-        sc.put("MethodsExample", sc.runScriptlet("Java::MethodsExample"));
-        sc.runScriptlet("class MethodsExample ; def my_ruby_method(msg) ; puts \"my_ruby_method called: #{msg}\" ; end ; end");
+        sc.put("JRUBY_6762", sc.runScriptlet("Java::JRUBY_6762"));
+        sc.runScriptlet("class JRUBY_6762 ; def my_ruby_method(msg) ; puts \"my_ruby_method called: #{msg}\" ; end ; end");
 
         sc.put("$java_instance", me);
 
         // Should be called since a Ruby method is defined
-        if ((Boolean) sc.runScriptlet("MethodsExample.instance_methods(false).include?(:my_ruby_method)")) {
+        if ((Boolean) sc.runScriptlet("JRUBY_6762.instance_methods(false).include?(:my_ruby_method)")) {
             sc.put("$ruby_arg", "ruby msg");
             sc.runScriptlet("$java_instance.my_ruby_method($ruby_arg)");
         }
 
         // Should not be called
-        if ((Boolean) sc.runScriptlet("MethodsExample.instance_methods(false).include?(:my_java_method)")) {
+        if ((Boolean) sc.runScriptlet("JRUBY_6762.instance_methods(false).include?(:my_java_method)")) {
             sc.put("$java_arg", "java msg");
             sc.runScriptlet("$java_instance.my_java_method($java_arg)");
         }
 
         // Should not be called
-        if ((Boolean) sc.runScriptlet("MethodsExample.instance_methods(false).include?(:myJavaMethod)")) {
+        if ((Boolean) sc.runScriptlet("JRUBY_6762.instance_methods(false).include?(:myJavaMethod)")) {
             sc.put("$java_arg", "java msg");
             sc.runScriptlet("$java_instance.myJavaMethod($java_arg)");
         }
