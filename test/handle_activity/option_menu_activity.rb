cbf4f2b17c3dd4beb6b14c6b37cb2b35d1eb9f24
Uwe Kubosch <uwe@kubosch.no>
2012-07-23 01:33:13 +0200

    * Added more tracing info about stack

--- test/handle_activity/option_menu_activity.rb
+++ test/handle_activity/option_menu_activity.rb
@@ -1,3 +1,7 @@
+STACK_DEPTH_SCRIPT = java.lang.Thread.current_thread.stack_trace.length.to_s
+
+raise "Stack level: #{STACK_DEPTH_SCRIPT}" rescue puts $!.message + "\n" + $!.backtrace.join("\n")
+
 require 'ruboto'
 
 ruboto_import_widgets :ImageButton, :LinearLayout, :TextView
