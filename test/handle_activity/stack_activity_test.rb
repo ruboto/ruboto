d459a5881249386e4eecb513b3c94c15095bab29
Uwe Kubosch <uwe@kubosch.no>
2012-07-28 15:47:26 +0200

    * Updated stack levels for standalone master

--- test/handle_activity/stack_activity_test.rb
+++ test/handle_activity/stack_activity_test.rb
@@ -21,12 +21,15 @@ test('stack depth') do |activity|
     jruby_offset = {
         '1.7.0.dev' => [1, 1, 1, 1],
         '1.7.0.preview1' => [0, -1, -1, -1],
-        '1.7.0.preview2.dev' => [0, 1, 4, 4],
+        '1.7.0.preview2.dev' => [0, -1, -1, -1],
     }[org.jruby.runtime.Constants::VERSION] || [0, 0, 0, 0]
   end
   version_message ="ANDROID: #{android.os.Build::VERSION::SDK_INT}, PLATFORM: #{org.ruboto.JRubyAdapter.uses_platform_apk ? org.ruboto.JRubyAdapter.platform_version_name : 'STANDALONE'}, JRuby: #{org.jruby.runtime.Constants::VERSION}"
-  assert_equal 43 + os_offset + jruby_offset[0], activity.find_view_by_id(42).text.to_i, version_message
-  assert_equal 67 + os_offset + jruby_offset[1], activity.find_view_by_id(43).text.to_i, version_message
-  assert_equal 76 + os_offset + jruby_offset[2], activity.find_view_by_id(44).text.to_i, version_message
-  assert_equal 92 + os_offset + jruby_offset[3], activity.find_view_by_id(45).text.to_i, version_message
+  assert_equal [43 + os_offset + jruby_offset[0],
+                67 + os_offset + jruby_offset[1],
+                76 + os_offset + jruby_offset[2],
+                92 + os_offset + jruby_offset[3]], [activity.find_view_by_id(42).text.to_i,
+                                                    activity.find_view_by_id(43).text.to_i,
+                                                    activity.find_view_by_id(44).text.to_i,
+                                                    activity.find_view_by_id(45).text.to_i], version_message
 end
