60e3fefe55bbb8b0b66d7be2fcad889d219c3e27
Uwe Kubosch <uwe@kubosch.no>
2012-07-31 11:54:30 +0200

    * Minor correction to margins test setup

--- test/activity/margins_activity_test.rb
+++ test/activity/margins_activity_test.rb
@@ -6,7 +6,7 @@ setup do |activity|
     @text_view_margins = activity.findViewById(42)
     @text_view_layout = activity.findViewById(43)
     @text_view_fieldset = activity.findViewById(44)
-    break if @text_view_margins || @text_view_layout || @text_view_fieldset || (Time.now - start > 60)
+    break if (@text_view_margins && @text_view_layout && @text_view_fieldset) || (Time.now - start > 60)
     sleep 1
   end
   assert @text_view_margins
