95d5f9b18fea7644c7832d2f69bd8af1949a999b
Uwe Kubosch <uwe@kubosch.no>
2012-08-20 13:50:12 +0200

    * Try to find correct stack level, again.

--- test/handle_activity/stack_activity_test.rb
+++ test/handle_activity/stack_activity_test.rb
@@ -10,12 +10,14 @@ setup do |activity|
   assert @text_view
 end
 
+# ANDROID: 15, PLATFORM: 0.4.8.dev, JRuby: 1.7.0.preview2 '[44, 68, 77, 93]' expected, but got '[44, 67, 76, 92]'
+
 test('stack depth') do |activity|
   os_offset = {13 => 1, 15 => 1, 16 => 1}[android.os.Build::VERSION::SDK_INT].to_i
   if org.ruboto.JRubyAdapter.uses_platform_apk?
     jruby_offset = {
         '0.4.7' => [0, -1, -1, -1],
-        '0.4.8.dev' => [0, -1, -1, -1],
+        '0.4.8.dev' => [0, -2, -2, -2],
     }[org.ruboto.JRubyAdapter.platform_version_name] || [0, 0, 0, 0]
   else # STANDALONE
     jruby_offset = {
@@ -25,11 +27,14 @@ test('stack depth') do |activity|
     }[org.jruby.runtime.Constants::VERSION] || [0, 0, 0, 0]
   end
   version_message ="ANDROID: #{android.os.Build::VERSION::SDK_INT}, PLATFORM: #{org.ruboto.JRubyAdapter.uses_platform_apk ? org.ruboto.JRubyAdapter.platform_version_name : 'STANDALONE'}, JRuby: #{org.jruby.runtime.Constants::VERSION}"
-  assert_equal [43 + os_offset + jruby_offset[0],
-                68 + os_offset + jruby_offset[1],
-                77 + os_offset + jruby_offset[2],
-                93 + os_offset + jruby_offset[3]], [activity.find_view_by_id(42).text.to_i,
-                                                    activity.find_view_by_id(43).text.to_i,
-                                                    activity.find_view_by_id(44).text.to_i,
-                                                    activity.find_view_by_id(45).text.to_i], version_message
+  expected = [43 + os_offset + jruby_offset[0],
+              68 + os_offset + jruby_offset[1],
+              77 + os_offset + jruby_offset[2],
+              93 + os_offset + jruby_offset[3]]
+  actual = [activity.find_view_by_id(42).text.to_i,
+            activity.find_view_by_id(43).text.to_i,
+            activity.find_view_by_id(44).text.to_i,
+            activity.find_view_by_id(45).text.to_i]
+  assert_equal expected, actual, version_message
+  puts "handle stack: #{version_message} #{actual.inspect}"
 end
