5be57658e76325a41232c4be34588f68d8bbb163
Uwe Kubosch <uwe@kubosch.no>
2012-09-22 12:49:41 +0200

    * Test subclassing with current JRuby 1.7.0.preview2 from master

--- test/app_test_methods.rb
+++ test/app_test_methods.rb
@@ -41,7 +41,7 @@ module AppTestMethods
       snake_name    = test_src.chomp('_test.rb')
 
       # FIXME(uwe):  Remove when we stop testing JRuby < 1.7.0.rc1
-      next if snake_name =~ /generate|subclass/ && (RUBOTO_PLATFORM == 'CURRENT' || JRUBY_JARS_VERSION < Gem::Version.new('1.7.0.rc1'))
+      next if snake_name =~ /subclass/ && (RUBOTO_PLATFORM == 'CURRENT' || JRUBY_JARS_VERSION < Gem::Version.new('1.7.0.preview2'))
 
       activity_name = File.basename(snake_name).split('_').map { |s| "#{s[0..0].upcase}#{s[1..-1]}" }.join
       Dir.chdir APP_DIR do
