40208367454446b19ec446601dfae28d0287ddf7
Uwe Kubosch <uwe@kubosch.no>
2012-11-09 16:24:27 +0100

    * Added test case for defining custom initialize (constructor) method when subclassing a Java class.

--- test/activity/subclass_activity_test.rb
+++ test/activity/subclass_activity_test.rb
@@ -16,3 +16,17 @@ test('item click sets text') do |activity|
   activity.run_on_ui_thread { @list_view.performItemClick(@list_view, 1, 1) }
   assert_equal '[Record one]', @text_view.text
 end
+
+class MyObject < java.lang.Object
+  attr_reader :my_param
+
+  def initialize(my_param)
+    super()
+    @my_param = my_param
+  end
+end
+
+test('add constructor with parameter') do
+  o = MyObject.new('It works!')
+  assert_equal 'It works!', o.my_param
+end
