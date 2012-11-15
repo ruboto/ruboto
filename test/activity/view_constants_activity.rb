d6bfe0664ee3d5b4b38cd792ad60903533b8f97f
Uwe Kubosch <uwe@kubosch.no>
2012-11-16 00:43:47 +0100

    * Moved view constants test into activity tests to speed up test runs.

---
+++ test/activity/view_constants_activity.rb
@@ -0,0 +1,42 @@
+require 'ruboto/activity'
+require 'ruboto/widget'
+
+ruboto_import_widgets :Button, :LinearLayout, :TextView
+
+class ViewConstantsActivity
+  def on_create(bundle)
+    super
+    $ruboto_test_app_activity = self
+    setTitle File.basename(__FILE__).chomp('_activity.rb').split('_').map { |s| "#{s[0..0].upcase}#{s[1..-1]}" }.join(' ')
+
+    self.content_view =
+        linear_layout :orientation => :vertical do
+          @text_view = text_view :text    => 'What hath Matz wrought?', :id => 42, :width => :fill_parent,
+                                 :gravity => android.view.Gravity::CENTER, :text_size => 48.0
+          button :text => 'M-x butterfly', :width => :fill_parent, :id => 43, :on_click_listener => proc { butterfly }
+        end
+  rescue
+    puts "Exception creating activity: \#{$!}"
+    puts $!.backtrace.join("\\n")
+  end
+
+  def set_text(text)
+    @text_view.text = text
+  end
+
+  private
+
+  def butterfly
+    puts 'butterfly'
+    Thread.start do
+      begin
+        startService(android.content.Intent.new(application_context, $package.RubotoTestService.java_class))
+      rescue Exception
+        puts "Exception starting the service: \#{$!}"
+        puts $!.backtrace.join("\\n")
+      end
+    end
+    puts 'butterfly OK'
+  end
+
+end
---
+++ test/activity/view_constants_activity_test.rb
@@ -0,0 +1,30 @@
+activity Java::org.ruboto.test_app.ViewConstantsActivity
+java_import "android.view.ViewGroup"
+java_import "android.view.Gravity"
+java_import "android.os.Build"
+
+setup do |activity|
+  start = Time.now
+  loop do
+    @text_view = activity.findViewById(42)
+    break if @text_view || (Time.now - start > 60)
+    sleep 1
+  end
+  assert @text_view
+end
+
+def view_constant(const)
+  View.convert_constant(const.downcase.to_sym)
+end
+
+test('LayoutParams Constants') do |activity|
+  ViewGroup::LayoutParams.constants.each do |const|
+    assert_equal ViewGroup::LayoutParams.const_get(const), view_constant(const)
+  end
+end
+
+test('Gravity Constants') do |activity|
+  Gravity.constants.each do |const|
+    assert_equal Gravity.const_get(const), view_constant(const)
+  end
+end
