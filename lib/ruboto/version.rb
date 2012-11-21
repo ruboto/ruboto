58939cdd3807412368bac0e8f28085b9cb6e5db3
Uwe Kubosch <uwe@kubosch.no>
2012-11-21 07:59:09 +0100

    * Bumped version to 0.10.0.rc.0 for release candidate.

--- lib/ruboto/version.rb
+++ lib/ruboto/version.rb
@@ -5,6 +5,6 @@ Android using the Ruby language and libraries.  It includes support libraries
 and generators for creating projects, classes, tests, and more.  The complete
 APIs of Android, Java, and Ruby is available to you using the Ruby language.
 EOF
-  VERSION = '0.10.0.dev'
+  VERSION = '0.10.0.rc.0'
   UPDATE_VERSION_LIMIT = '0.7.0'
 end
