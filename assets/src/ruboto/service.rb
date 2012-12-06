1cb240096c9fbf39eb35c1c4e1fd1442e4b5eb19
rscottm <rscottmoyer@gmail.com>
2012-12-06 13:36:06 -0800

    Fixed up like activity.rb (options, intent)

--- assets/src/ruboto/service.rb
+++ assets/src/ruboto/service.rb
@@ -14,7 +14,18 @@ java_import "org.ruboto.RubotoService"
 
 module Ruboto
   module Context
-    def start_ruboto_service(global_variable_name = '$service', klass=RubotoService, &block)
+    def start_ruboto_service(global_variable_name='$service', klass=RubotoService, options={}, &block)
+      # FIXME(uwe): Translate old positional signature to new options-based signature.
+      # FIXME(uwe): Remove when we stop supporting Ruboto 0.8.0 or older.
+      if options.nil?
+        if global_variable_name.is_a?(Hash)
+          options = global_variable_name
+        else
+          options = {}
+        end
+        global_variable_name = nil
+      end
+
       class_name = options[:class_name] || "#{klass.name.split('::').last}_#{source_descriptor(block)[0].split("/").last.gsub(/[.-]+/, '_')}_#{source_descriptor(block)[1]}"
       if !Object.const_defined?(class_name)
         Object.const_set(class_name, Class.new(&block))
@@ -22,12 +33,12 @@ module Ruboto
         Object.const_get(class_name).class_eval(&block) if block_given?
       end
       b = Java::android.os.Bundle.new
-      b.putInt("Theme", theme) if theme
       b.putString("ClassName", class_name)
+      b.putString("Script", options[:script]) if options[:script]
       i = android.content.Intent.new
       i.setClass self, klass.java_class
-      i.putExtra("RubotoActivity Config", b)
-      self.startService Java::android.content.Intent.new(self, klass.java_class)
+      i.putExtra("Ruboto Config", b)
+      self.startService i
       self
     end
   end
