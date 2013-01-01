75964b6b7af0e031e1b5aa9a458a14f16fb83156
Uwe Kubosch <uwe@kubosch.no>
2013-01-01 19:13:34 +0100

    * JRuby suddenly much larger...

--- test/minimal_app_test.rb
+++ test/minimal_app_test.rb
@@ -13,11 +13,12 @@ if RubotoTest::RUBOTO_PLATFORM == 'STANDALONE'
       cleanup_app
     end
 
+    # APK was larger than 3.2MB: 3.5MB.  JRuby: 1.6.7,          ANDROID_TARGET: 15.
     # APK was larger than 3.2MB: 3.3MB.  JRuby: 1.6.7.2,        ANDROID_TARGET: 10.
     # APK was larger than 4.4MB: 4.7MB.  JRuby: 1.7.0.preview2, ANDROID_TARGET: 10.
-    # APK was larger than 3.2MB: 3.5MB.  JRuby: 1.6.7,          ANDROID_TARGET: 15.
     # APK was larger than 4.6MB: 4.9MB.  JRuby: 1.7.0.preview2, ANDROID_TARGET: 15.
     # APK was larger than 3.2MB: 4.7MB.  JRuby: 1.7.0,          ANDROID_TARGET: 15.
+    # APK was larger than 4.9MB: 7.2MB.  JRuby: 1.7.2.dev,      ANDROID_TARGET: 10.
 
     def test_minimal_apk_is_less_than_3_mb
       apk_size = BigDecimal(File.size("#{APP_DIR}/bin/RubotoTestApp-debug.apk").to_s) / (1024 * 1024)
@@ -27,6 +28,7 @@ if RubotoTest::RUBOTO_PLATFORM == 'STANDALONE'
           '1.6.8' => 3.5,
           '1.7.0' => ANDROID_TARGET < 15 ? 4.7 : 4.9,
           '1.7.1.dev' => ANDROID_TARGET < 15 ? 4.7 : 4.9,
+          '1.7.2.dev' => 7.2,
       }[JRUBY_JARS_VERSION.to_s] || 4.9
       lower_limit = upper_limit * 0.9
       version_message ="JRuby: #{JRUBY_JARS_VERSION}, ANDROID_TARGET: #{ANDROID_TARGET}"
