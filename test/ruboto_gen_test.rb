44e3541504d70a32daf0bf9bbbf39f22fa00fa55
Uwe Kubosch <uwe@kubosch.no>
2013-03-03 11:10:03 +0100

    * Allow bigger APK for JRuby 1.7.4.dev

--- test/ruboto_gen_test.rb
+++ test/ruboto_gen_test.rb
@@ -58,6 +58,8 @@ class RubotoGenTest < Test::Unit::TestCase
   # APK was  7310.1KB.  PLATFORM: STANDALONE, ANDROID_TARGET: 10, JRuby: 1.7.2
   # APK was  7336.2KB.  PLATFORM: STANDALONE, ANDROID_TARGET: 10, JRuby: 1.7.3.dev
   # APK was  7316.5KB.  PLATFORM: STANDALONE, ANDROID_TARGET: 15, JRuby: 1.7.3.dev
+  # APK was  7405.8KB.  PLATFORM: STANDALONE, ANDROID_TARGET: 15, JRuby: 1.7.4.dev
+  # APK was  7420.9KB.  PLATFORM: STANDALONE, ANDROID_TARGET: 16, JRuby: 1.7.4.dev
   def test_new_apk_size_is_within_limits
     apk_size = BigDecimal(File.size("#{APP_DIR}/bin/RubotoTestApp-debug.apk").to_s) / 1024
     version = "  PLATFORM: #{RUBOTO_PLATFORM}"
@@ -69,7 +71,8 @@ class RubotoGenTest < Test::Unit::TestCase
           '1.7.0' => ANDROID_TARGET < 15 ? 7400.0 : 7600.0,
           '1.7.1' => ANDROID_TARGET < 15 ? 7400.0 : 7600.0,
           '1.7.2' => ANDROID_TARGET < 15 ? 7400.0 : 9970.0,
-      }[JRUBY_JARS_VERSION.to_s] || 7400.0
+          '1.7.3' => ANDROID_TARGET < 15 ? 7400.0 : 9970.0,
+      }[JRUBY_JARS_VERSION.to_s] || 7500.0
       version << ", JRuby: #{JRUBY_JARS_VERSION.to_s}"
     else
       upper_limit = {
