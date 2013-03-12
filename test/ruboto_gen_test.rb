e1893620a7e21d8ed3ae0d3f63f188259cdb7fab
Uwe Kubosch <uwe@kubosch.no>
2013-03-12 13:33:15 +0100

    * Slightly larger APK for Android 4.1.

--- test/ruboto_gen_test.rb
+++ test/ruboto_gen_test.rb
@@ -52,7 +52,7 @@ class RubotoGenTest < Test::Unit::TestCase
   # APK was    56.4KB.  PLATFORM: CURRENT,    ANDROID_TARGET: 7
   # APK was    61.9KB.  PLATFORM: CURRENT,    ANDROID_TARGET: 10
   # APK was    67.2KB.  PLATFORM: CURRENT,    ANDROID_TARGET: 15
-  # APK was    72.9KB.  PLATFORM: CURRENT     ANDROID_TARGET: 16
+  # APK was    73.1KB.  PLATFORM: CURRENT,    ANDROID_TARGET: 16
   # APK was  5879.2KB.  PLATFORM: STANDALONE, ANDROID_TARGET: 15, JRuby: 1.6.7
   # APK was  7380.0KB.  PLATFORM: STANDALONE, ANDROID_TARGET: 15, JRuby: 1.7.0
   # APK was  7310.1KB.  PLATFORM: STANDALONE, ANDROID_TARGET: 10, JRuby: 1.7.2
@@ -80,7 +80,8 @@ class RubotoGenTest < Test::Unit::TestCase
           7 => 62.0,
           10 => 62.0,
           15 => 63.0,
-      }[ANDROID_TARGET] || 73.0
+          16 => 74.0,
+      }[ANDROID_TARGET] || 74.0
     end
     lower_limit = upper_limit * 0.9
     assert apk_size <= upper_limit, "APK was larger than #{'%.1f' % upper_limit}KB: #{'%.1f' % apk_size.ceil(1)}KB.#{version}"
