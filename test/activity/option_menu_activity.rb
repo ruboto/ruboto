1f169948253c85aacdb4b834e1eb6d9714078c13
Uwe Kubosch <uwe@kubosch.no>
2013-05-10 21:58:29 +0200

    * Fix option menu test.  Not sure why it is failing.

--- test/activity/option_menu_activity.rb
+++ test/activity/option_menu_activity.rb
@@ -4,16 +4,14 @@ require 'ruboto/util/toast'
 ruboto_import_widgets :LinearLayout, :TextView
 
 class OptionMenuActivity
-  attr_reader :text_view_widget
-
   def onCreate(bundle)
     super
     set_title File.basename(__FILE__).chomp('_activity.rb').split('_').map { |s| "#{s[0..0].upcase}#{s[1..-1]}" }.join(' ')
 
     self.content_view =
         linear_layout :orientation => LinearLayout::VERTICAL do
-          @text_view_widget = text_view :text  => 'What hath Matz wrought?', :id => 42, :text_size => 48.0,
-                                 :width => :fill_parent, :gravity => android.view.Gravity::CENTER
+          text_view :text => 'What hath Matz wrought?', :id => 42, :text_size => 48.0,
+                    :width => :fill_parent, :gravity => android.view.Gravity::CENTER
         end
   end
 
@@ -21,7 +19,7 @@ class OptionMenuActivity
     mi = menu.add('Test')
     mi.setIcon($package.R::drawable::get_ruboto_core)
     mi.set_on_menu_item_click_listener do |menu_item|
-      @text_view_widget.text = 'What hath Matz wrought!'
+      findViewById(42).text = 'What hath Matz wrought!'
       toast 'Flipped a bit via butterfly'
     end
     true
