e918dba0285be51ff158bba606437ea958d58068
Uwe Kubosch <uwe@kubosch.no>
2013-08-31 01:01:05 +0200

    * Removed test for stdlib exclusion to speed up tests.  stdlib _inclusion_ is still tested. * Issue #466 Need faster tests for Travis-CI

--- test/minimal_app_test.rb
+++ test/minimal_app_test.rb
@@ -40,30 +40,5 @@ if RubotoTest::RUBOTO_PLATFORM == 'STANDALONE'
       run_app_tests
     end
 
-    # APK was 4.7MB.  JRuby: 1.7.0, ANDROID_TARGET: 15
-    # APK was 4.5MB.  JRuby: 1.7.2, ANDROID_TARGET: 10
-    # APK was 4.5MB.  JRuby: 1.7.2, ANDROID_TARGET: 15
-    # APK was 4.5MB.  JRuby: 1.7.3, ANDROID_TARGET: 10
-    # APK was 4.5MB.  JRuby: 1.7.3, ANDROID_TARGET: 15
-    # APK was 4.9MB.  JRuby: 1.7.4, ANDROID_TARGET: 10
-    # APK was 5.1MB.  JRuby: 1.7.5.dev, ANDROID_TARGET: 15
-    # FIXME(uwe): Remove when we remove the exclude feature
-    def test_minimal_apk_with_excludes_is_less_than_5_mb
-      generate_app :excluded_stdlibs => %w{ant cgi digest dl drb ffi irb net optparse racc rbconfig rdoc rexml rinda rss
-        rubygems runit shell soap test uri webrick win32 wsdl xmlrpc xsd ../1.9}
-      apk_size = BigDecimal(File.size("#{APP_DIR}/bin/RubotoTestApp-debug.apk").to_s) / (1024 * 1024)
-      upper_limit = {
-          '1.7.0' => ANDROID_TARGET < 15 ? 4.7 : 4.9,
-          '1.7.1' => ANDROID_TARGET < 15 ? 4.7 : 4.9,
-          '1.7.2' => ANDROID_TARGET < 15 ? 4.6 : 4.9,
-          '1.7.3' => ANDROID_TARGET < 15 ? 4.6 : 4.9,
-          '1.7.4' => ANDROID_TARGET < 15 ? 4.9 : 5.1,
-      }[JRUBY_JARS_VERSION.to_s] || {10 => 5.1, 15 => 5.1, 16 => 5.1}[ANDROID_TARGET]
-      lower_limit = upper_limit * 0.9
-      version_message ="JRuby: #{JRUBY_JARS_VERSION}, ANDROID_TARGET: #{ANDROID_TARGET}"
-      assert apk_size <= upper_limit, "APK was larger than #{'%.1f' % upper_limit}MB: #{'%.1f' % apk_size.ceil(1)}MB.  #{version_message}"
-      assert apk_size >= lower_limit, "APK was smaller than #{'%.1f' % lower_limit}MB: #{'%.1f' % apk_size.floor(1)}MB.  You should lower the limit.  #{version_message}"
-    end
-
   end
 end
