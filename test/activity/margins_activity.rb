bb9d32ab516b132d1bd06f62cea329098ee92410
Scott Moyer <rscottmoyer@gmail.com>
2013-11-29 00:59:54 -0800

    Revert test to allow snake case parameters

--- test/activity/margins_activity.rb
+++ test/activity/margins_activity.rb
@@ -11,7 +11,7 @@ class MarginsActivity
         linear_layout :orientation => LinearLayout::VERTICAL do
           @text_view_margins = text_view :text => 'What hath Matz wrought?', :id => 42, :margins => [100,0,0,0]
           @text_view_layout = text_view :text => 'What hath Matz wrought?', :id => 43, :layout => {:set_margins => [100,0,0,0]}
-          @text_view_fieldset = text_view :text => 'What hath Matz wrought?', :id => 44, :layout => {:leftMargin => 100}
+          @text_view_fieldset = text_view :text => 'What hath Matz wrought?', :id => 44, :layout => {:left_margin => 100}
         end
   end
 end
