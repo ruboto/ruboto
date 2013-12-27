2c122c0e4a4e74cc74424df5d8878ee95876cae0
Uwe Kubosch <uwe@kubosch.no>
2013-12-27 14:52:17 +0100

    * Test JRuby 1.7.9 and MRI 2.1

--- test/ruboto_gen_test.rb
+++ test/ruboto_gen_test.rb
@@ -60,6 +60,7 @@ class RubotoGenTest < Test::Unit::TestCase
   # APK was  8770.2KB.  PLATFORM: STANDALONE, ANDROID_TARGET: 16, JRuby: 1.7.5
   # APK was  8766.1KB.  PLATFORM: STANDALONE, ANDROID_TARGET: 16, JRuby: 1.7.6
   # APK was  8781.6KB.  PLATFORM: STANDALONE, ANDROID_TARGET: 16, JRuby: 1.7.8
+  # APK was  8789.6KB.  PLATFORM: STANDALONE, ANDROID_TARGET: 16, JRuby: 1.7.9
   # APK was  6689.5KB.  PLATFORM: STANDALONE, ANDROID_TARGET: 10, JRuby: 9000.dev
   # APK was  6962.1KB.  PLATFORM: STANDALONE, ANDROID_TARGET: 16, JRuby: 9000.dev
   def test_new_apk_size_is_within_limits
@@ -72,6 +73,7 @@ class RubotoGenTest < Test::Unit::TestCase
           '1.7.5' => 8800.0,
           '1.7.6' => 8800.0,
           '1.7.8' => 8800.0,
+          '1.7.9' => 8800.0,
           '9000.dev' => 7000.0,
       }[JRUBY_JARS_VERSION.to_s] || 0.0
       version << ", JRuby: #{JRUBY_JARS_VERSION.to_s}"
