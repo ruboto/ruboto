f9234c50989d15748d50b3f02f3866b5819e0118
Uwe Kubosch <uwe@kubosch.no>
2013-12-27 11:22:04 +0100

    * Closes #546 Better stack traces using "jruby.rewrite.java.trace" = "true"

--- assets/src/org/ruboto/JRubyAdapter.java
+++ assets/src/org/ruboto/JRubyAdapter.java
@@ -132,17 +132,18 @@ public class JRubyAdapter {
             // END Ruboto HeapAlloc
             setDebugBuild(appContext);
             Log.d("Setting up JRuby runtime (" + (isDebugBuild ? "DEBUG" : "RELEASE") + ")");
-            System.setProperty("jruby.compile.mode", "OFF"); // OFF OFFIR JITIR? FORCE FORCEIR
-            // System.setProperty("jruby.compile.backend", "DALVIK");
-            System.setProperty("jruby.bytecode.version", "1.6");
-            System.setProperty("jruby.interfaces.useProxy", "true");
-            System.setProperty("jruby.management.enabled", "false");
-            System.setProperty("jruby.objectspace.enabled", "false");
-            System.setProperty("jruby.thread.pooling", "true");
-            System.setProperty("jruby.native.enabled", "false");
-            // System.setProperty("jruby.compat.version", "RUBY2_0"); // RUBY1_9 is the default in JRuby 1.7
-            System.setProperty("jruby.ir.passes", "LocalOptimizationPass,DeadCodeElimination");
             System.setProperty("jruby.backtrace.style", "normal"); // normal raw full mri
+            System.setProperty("jruby.bytecode.version", "1.6");
+            // System.setProperty("jruby.compat.version", "RUBY2_0"); // RUBY1_9 is the default in JRuby 1.7
+            // System.setProperty("jruby.compile.backend", "DALVIK");
+            System.setProperty("jruby.compile.mode", "OFF"); // OFF OFFIR JITIR? FORCE FORCEIR
+            System.setProperty("jruby.interfaces.useProxy", "true");
+            System.setProperty("jruby.ir.passes", "LocalOptimizationPass,DeadCodeElimination");
+            System.setProperty("jruby.management.enabled", "false");
+            System.setProperty("jruby.native.enabled", "false");
+            System.setProperty("jruby.objectspace.enabled", "false");
+            System.setProperty("jruby.rewrite.java.trace", "true");
+            System.setProperty("jruby.thread.pooling", "true");
 
             // Uncomment these to debug/profile Ruby source loading
             // System.setProperty("jruby.debug.loadService", "true");
