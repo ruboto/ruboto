92db1912dc2e94670b5fa5b8d59522b015d35c4e
rscottm <rscottmoyer@gmail.com>
2014-07-20 18:53:05 -0700

    (#648) Added preOnStartCommand and preOnBind; condition preOnCreate to only function if not using RubotoService

--- assets/src/RubotoService.java
+++ assets/src/RubotoService.java
@@ -9,8 +9,24 @@ public class THE_RUBOTO_CLASS THE_ACTION THE_ANDROID_CLASS {
      * Called at the start of onCreate() to prepare the Activity.
      */
     private void preOnCreate() {
-        System.out.println("RubotoService onCreate(): " + getClass().getName());
-        getScriptInfo().setRubyClassName(getClass().getSimpleName());
+        if (!getClass().getSimpleName().equals("RubotoService")) {
+          System.out.println("RubotoService onCreate(): " + getClass().getName());
+          getScriptInfo().setRubyClassName(getClass().getSimpleName());
+        }
+    }
+
+    private void preOnStartCommand(android.content.Intent intent) {
+        if (getClass().getSimpleName().equals("RubotoService")) {
+          System.out.println("RubotoService onStartCommand(): " + getClass().getName());
+          scriptInfo.setFromIntent(intent);
+        }
+    }
+
+    private void preOnBind(android.content.Intent intent) {
+        if (getClass().getSimpleName().equals("RubotoService")) {
+          System.out.println("RubotoService onBind(): " + getClass().getName());
+          scriptInfo.setFromIntent(intent);
+        }
     }
 
 THE_METHODS
