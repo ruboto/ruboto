0e1cb50f3b7f454ec2b5f3e63891f2929fef896d
Uwe Kubosch <uwe@kubosch.no>
2014-10-12 04:06:17 +0200

    * Set largeHeap in the emulator if we are outside a project.

--- lib/ruboto/util/emulator.rb
+++ lib/ruboto/util/emulator.rb
@@ -85,7 +85,7 @@ module Ruboto
 
           avd_home = "#{ENV['HOME'].gsub('\\', '/')}/.android/avd/#{avd_name}.avd"
           manifest_file = 'AndroidManifest.xml'
-          large_heap = (File.exists?(manifest_file) && File.read(manifest_file) =~ /largeHeap/)
+          large_heap = !File.exists?(manifest_file) || File.read(manifest_file) =~ /largeHeap/
           heap_size = large_heap ? 256 : 48
 
           unless File.exists? avd_home
