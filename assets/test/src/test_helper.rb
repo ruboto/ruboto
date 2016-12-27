e31ac32c6e65b2b3cf1894e636b97d8d08776f73
Uwe Kubosch <uwe@kubosch.no>
2016-12-27 21:03:31 +0100

    Fixes #826 Rename `assert_matches` to `assert_match` for Minitest compatibility

--- assets/test/src/test_helper.rb
+++ assets/test/src/test_helper.rb
@@ -16,6 +16,10 @@ def assert_less_than_or_equal(limit, actual, message = nil)
   raise "#{"#{message}\n" if message}Expected '#{actual}' to be less than or equal to '#{limit}'" unless actual <= limit
 end
 
-def assert_matches(pattern, actual, message = nil)
+def assert_match(pattern, actual, message = nil)
   raise "#{"#{message}\n" if message}'#{pattern}' expected, but got '#{actual}'" unless pattern =~ actual
+end
+def assert_matches(*args)
+  warn '"assert_matches" is deprecated.  Use "assert_match" instead, for Minitest compatibility.'
+  assert_match(*args)
 end
